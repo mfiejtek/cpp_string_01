f6122abea55f1fdcab238cb84b8900f12540832b
fiejtek_1162023 <fiejtek_1162023@mfiejtek.mfiejtek.virtualbox.org>
2024-04-14 19:45:48 +0200

    Rozwiazanie

--- string_01.cc
+++ string_01.cc
@@ -1,3 +1,31 @@
 #include "string_01.h"
+#include <iostream>
 
-auto main() -> int { return 0; }
+
+
+auto main() -> int { 
+    //std::cout<<"Podaj string: ";
+    std::string input;
+    std::getline(std::cin, input);
+    //std::cout<<"Podaj substring: ";
+    std::string substr;
+    std::getline(std::cin, substr);
+
+    // std::cout<<"Reverse: "<<string_toys::reverse(input)<<std::endl;
+    string_toys::reverse(input);
+    // std::cout<<"First to upper: "<<string_toys::first_to_upper(input)<<std::endl;
+    string_toys::first_to_upper(input);
+    // std::cout<<"Vowels:  "<<string_toys::count_vowels(input)<<std::endl;
+    string_toys::count_vowels(input);
+    // std::cout<<"Suma:  "<<string_toys::sum_digits(input)<<std::endl;
+    string_toys::sum_digits(input);
+    // std::cout<<"Substr:  "<<string_toys::search_substr(input.c_str(), substr.c_str())<<std::endl;
+    string_toys::search_substr(input, substr);
+    // std::cout<<"Substr:  "<<string_toys::custom_serach(input.c_str(), substr.c_str())<<std::endl;
+    string_toys::custom_serach(input.c_str(), substr.c_str());
+    
+    
+    
+    
+    return 0; 
+}
--- string_01.h
+++ string_01.h
@@ -1,31 +1,109 @@
 #ifndef CPP_STRING_H
 #define CPP_STRING_H
 
+#include <string>
+#include <sstream>
+#include <cctype>
 
 namespace string_toys {
 
 auto reverse(const std::string& str) -> std::string {
+    std::string odw_string{str};
+    int dlugosc_stringa = str.length();
+    for(int i=0; i<dlugosc_stringa; ++i){
+        odw_string[i] = str[dlugosc_stringa-i-1];
+    }
+    return odw_string;
 
 }
 
 auto first_to_upper(const std::string& str) -> std::string {
-
+    std::stringstream ss(str);
+    std::string word;
+    std::string ftu_string;
+    while(ss>>word){
+        word[0] = toupper(word[0]);
+        ftu_string += word + " ";
+    }
+    return ftu_string.substr(0, ftu_string.length()-1);
 }
 
 auto count_vowels(const std::string& str) -> int {
-    
+    int licznik = 0;
+    for(char c: str){
+        char lower = tolower(c);
+        if(lower == 'a' || lower == 'e' || lower == 'i' || lower == 'o' || lower == 'u'){
+            licznik++;
+        }
+    }
+    return licznik;
 }
 
 auto sum_digits(const std::string& str) -> int {
-    
+    std::stringstream ss(str);
+    std::string word;
+    int suma = 0;
+
+    while(ss>>word){
+        for(char c: word){
+            if(isdigit(c)){
+                suma += c - '0';
+            }
+        }    
+    }
+    return suma;
 }
 
 auto search_substr(const std::string& str, const std::string& substr) -> int {
+    int dlugosc_stringa = str.length();
+    int dlugosc_substr = substr.length();
+    
+    if(dlugosc_substr>dlugosc_stringa){
+        return -1;
+    } 
+
+    for(int i=0; i <= dlugosc_stringa - dlugosc_substr; ++i){
+        int j;
+        for(j=0; j < dlugosc_substr; ++j){
+            if(str[i+j] != substr[j]){
+                break;
+            }
+        }
+        if(j == dlugosc_substr){
+            return i;
+        }
+    }
+    return -1;
 
 }
 
 auto custom_serach(const char* str, const char* substr) -> int {
-    
+    int dlugosc_stringa = 0;
+    int dlugosc_substr = 0;
+
+    while(str[dlugosc_stringa]!='\0'){
+        dlugosc_stringa++;
+    }
+    while(substr[dlugosc_substr]!='\0'){
+        dlugosc_substr++;
+    }
+
+    if(dlugosc_substr>dlugosc_stringa){
+        return -1;
+    } 
+
+    for(int i=0; i <= dlugosc_stringa - dlugosc_substr; ++i){
+        int j;
+        for(j=0; j < dlugosc_substr; ++j){
+            if(str[i+j] != substr[j]){
+                break;
+            }
+        }
+        if(j == dlugosc_substr){
+            return i;
+        }
+    }
+    return -1;
 }
 
 }  // namespace string_toys
